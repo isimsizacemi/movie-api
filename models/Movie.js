20280f7694922f47b6e9a72f67a0c8a8154dce50
Your Name <you@example.com>
2023-10-06 23:01:34 +0300

    Muhammed abiye soracağım kısım

--- models/Movie.js
+++ models/Movie.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 
 const MovieSchema = new Schema({
     directory_id: Schema.Types.ObjectId,
+    director_id: {
+        type: Schema.Types.ObjectId,
+        ref:'Director'
+    },
     title: {
         type: String,
         require: true
--- routes/director.js
+++ routes/director.js
@@ -1,6 +1,6 @@
-var express = require('express');
-
-var router = express.Router();
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
 
 
 //models 
@@ -8,11 +8,75 @@ const Director = require('../models/Director');
 
 /* GET home page. */
 router.post('/', async function(req, res, next) {
-    const director = new Director();
+    const director = new Director(req.body);
 
     const promise = await director.save();
 
     res.json(promise);
 });
+
+router.get('/', async (req,res) => {
+    try{
+        const directors =  await Director.find({ });
+        res.json(directors);
+    }catch{
+        res.status(500).json({ error: 'Kayırlar Bulunamadı!' });
+    }
+})
+
+router.get('/:director_id',  (req, res) => {
+    const promise = Director.aggregate([
+        {
+            $match: {
+                '_id':  mongoose.Types.ObjectId(req.params.director_id.toString)
+            }
+        },
+        {
+            $lookup: {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies',
+                preserveNullAndEmptyArrays: true // Bu, yönetmenin hiç filmi olmayanları da içerir.
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                   
+                },
+                
+                movies: {
+                    $push: '$movies',
+                }
+                
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                movies : '$movies'
+            }
+        }
+    ]);
+
+    promise.then( () => {
+        res.json(promise);
+    }).catch((err) => {
+        res.status(500).json({ error: 'Yönetmenler ve filmler alınamadı.' });
+    });
+});
+
 
 module.exports = router;
